7859b8a3b0693b1f4c5a24d6ef195aa8d1d36a85
Pieter <vandyck.pieter@gmail.com>
2021-04-14 00:33:41 +0200

    Some dummy model to play with.

--- rondgang/models.py
+++ rondgang/models.py
@@ -1,3 +1,22 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+class Gemeente(models.Model):
+    naam_text = models.CharField(max_length=50)
+    deelgemeente_text = models.CharField(max_length=50)
+
+
+class Segment(models.Model):
+    gemeente = models.CharField(max_length=55)
+    straat = models.CharField(max_length=50)
+    commentaar = models.TextField()
+
+
+class Status(models.Model):
+    gemeente = models.CharField(max_length=20)
+    straat = models.CharField(max_length=50)
+    afgewerkt = models.BooleanField()
+    lid = models.CharField(max_length=50)
+    datum = models.DateField(default=date.today)
+    commentaar = models.TextField()
